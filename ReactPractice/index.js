e5cb9ac9421b79fa00b86f04be44e66b1f9eacbb
Sushma Sreedharan <sushmacs15@gmail.com>
2019-09-22 14:06:46 -0700

    react app

--- ReactPractice/index.js
+++ ReactPractice/index.js
@@ -1,9 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import chemElements from "./elements.json";
+import PeriodicTable from "./periodic.js";
 
+let cols = ["atomic_number", "name", "symbol", "phase", "year_of_discovery"];
+//
+//
+//const listItems = cols.map((cols) => <li>{listItems}</li>
+//);
+let contents = < div >
+    <h2>Brought to you by Sushma Cheneerkuppum sreedharan: rg4984</h2>
+    <p>There are 118 chemical elements</p>
+        <PeriodicTable chemElements = {chemElements} desiredCols = {cols}/> < /div > ;
 
-ReactDOM.render( <
-    h1 > The Periodic Table < /h1>,
-    document.getElementById("root")
-);
+ReactDOM.render(contents, document.getElementById("root"));
