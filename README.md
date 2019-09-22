ff93bf7c8e544a837af188a48539c5c0a62e5562
Sushma Sreedharan <sushmacs15@gmail.com>
2019-09-22 15:16:55 -0700

    periodic table

--- README.md
+++ README.md
@@ -60,3 +60,53 @@ ReactDOM.render( <
 
 </html>
 ```
+### (b)
+
+![Developer-tool Screenshot](images/periodicTable.png)
+
+```javaScript
+import React from "react";
+import ReactDOM from "react-dom";
+import chemElements from "./elements.json";
+
+function PeriodicTable(props) {
+    
+const row = props.chemElements.map((chemDetails,index)=> 
+                        <ul id="tableItems">
+                        <li>{chemDetails.atomic_number}</li>
+                        <li>{chemDetails.name}</li>
+                        <li>{chemDetails.symbol}</li>
+                        <li>{chemDetails.phase}</li>
+                        <li>{chemDetails.year_of_discovery}</li>
+                        </ul>);
+return (
+   <div>
+      <ul id = "tableHeader">{props.desiredCols.map((cols)=> <li>{cols}</li>)}</ul>
+      <div>{row}</div>
+   </div>
+);
+}
+      
+export default PeriodicTable;
+```
+```CSS
+#tableHeader li {
+    padding: 5px;
+    width: 13%;
+}
+
+#tableHeader {
+    display: flex;
+    list-style-type: none;
+}
+
+#tableItems {
+    display: flex;
+}
+
+#tableItems li {
+    padding: 5px;
+    list-style-type: none;
+    width: 13%;
+}
+```
--- ReactPractice/periodic.js
+++ ReactPractice/periodic.js
@@ -3,25 +3,21 @@ import ReactDOM from "react-dom";
 import chemElements from "./elements.json";
 
 function PeriodicTable(props) {
-            const row = props.chemElements.map((chemDetails,index)=> 
-                                               <ul id="tableItems">
-      
-    <li>{chemDetails.atomic_number}</li>
-    <li>{chemDetails.name}</li>
-    <li>{chemDetails.symbol}</li>
-    <li>{chemDetails.phase}</li>
-    <li>{chemDetails.year_of_discovery}</li>
-       
-    </ul>);
-    return (
-                                               
-      <div id="tableHeaderContainer">
-      <ul id = "tableHeader">{props.desiredCols.map((cols)=> <li>{cols}</li>)}
-      </ul>
-      <div id="tableContentContainer">{row}</div>
-</div>
-
-    );
+    
+const row = props.chemElements.map((chemDetails,index)=> 
+                        <ul id="tableItems">
+                        <li>{chemDetails.atomic_number}</li>
+                        <li>{chemDetails.name}</li>
+                        <li>{chemDetails.symbol}</li>
+                        <li>{chemDetails.phase}</li>
+                        <li>{chemDetails.year_of_discovery}</li>
+                        </ul>);
+return (
+   <div>
+      <ul id = "tableHeader">{props.desiredCols.map((cols)=> <li>{cols}</li>)}</ul>
+      <div>{row}</div>
+   </div>
+);
 }
       
 export default PeriodicTable;
